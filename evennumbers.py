8396929fa580bd678104c414b1a02f49b530a2e7
Vedanagapriyark <vedanagapriya@gmail.com>
2025-04-17 07:55:11 +0530

    Update evennumbers.py

--- evennumbers.py
+++ evennumbers.py
@@ -6,3 +6,7 @@ numbers = list(map(int, input("Enter numbers separated by space: ").split()))
 
 # Output
 print("Even Numbers:", filter_even_numbers(numbers))
+
+OUTPUT:
+Enter numbers separated by space: 2 34 0 9 8
+Even Numbers: [2, 34, 0, 8]
