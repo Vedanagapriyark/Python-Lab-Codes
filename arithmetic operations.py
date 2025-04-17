e4675bf9247a8a2848013ddb045f8a8666d579e0
Vedanagapriyark <vedanagapriya@gmail.com>
2025-04-17 07:51:24 +0530

    Update arithmetic operations.py

--- arithmetic operations.py
+++ arithmetic operations.py
@@ -13,3 +13,11 @@ print("Subtraction:", subtract(x, y))
 print("Multiplication:", multiply(x, y))
 print("Division:", divide(x, y))
 
+OUTPUT:
+Enter first number: 2
+Enter second number: 3
+Addition: 5.0
+Subtraction: -1.0
+Multiplication: 6.0
+Division: 0.6666666666666666
+
